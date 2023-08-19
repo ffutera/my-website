0c84beb7720ab624cf3fc265a59ca495699fd659
ffutera <ffutera@gmail.com>
2023-08-19 23:30:27 +0100

    improve js

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-(function () {
-    document.addEventListener("DOMContentLoaded", function () {
+(function() {
+    function initializeCategorySelection() {
         const categoryCards = document.querySelectorAll(".category-card");
         const eventCards = document.querySelector(".event-cards");
 
+        function updateCategoryDisplay(categoryName) {
+            categoryCards.forEach(card => {
+                card.classList.remove("selected");
+            });
+
+            const selectedCard = document.querySelector(`.${categoryName}`);
+            selectedCard.classList.add("selected");
+
+            eventCards.style.display = "block";
+            document.querySelectorAll(".event-card").forEach(card => {
+                card.style.display = "none";
+            });
+
+            const categoryEvents = document.querySelectorAll(`.${categoryName}-event`);
+            categoryEvents.forEach(event => {
+                event.style.display = "block";
+            });
+        }
+
         categoryCards.forEach(categoryCard => {
             categoryCard.addEventListener("click", function () {
-                categoryCards.forEach(card => {
-                    card.classList.remove("selected");
-                });
-
-                this.classList.add("selected");
-
-                eventCards.style.display = "block";
                 const categoryName = this.classList[1];
-                const categoryEvents = document.querySelectorAll(`.${categoryName}-event`);
-
-                document.querySelectorAll(".event-card").forEach(card => {
-                    card.style.display = "none";
-                });
-
-                categoryEvents.forEach(event => {
-                    event.style.display = "block";
-                });
+                updateCategoryDisplay(categoryName);
             });
         });
 
         const academicsCategoryCard = document.querySelector(".academics");
-        academicsCategoryCard.click();
-    });
+        updateCategoryDisplay("academics");
+    }
+
+    initializeCategorySelection();
 })();
