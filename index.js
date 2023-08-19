2ff1c96872cbd096c92d8a9b286e41017f1562b1
ffutera <ffutera@gmail.com>
2023-08-19 22:43:51 +0100

    stylistic changes and a color change for clicked category cards

--- index.js
+++ index.js
@@ -2,23 +2,29 @@
     document.addEventListener("DOMContentLoaded", function () {
         const categoryCards = document.querySelectorAll(".category-card");
         const eventCards = document.querySelector(".event-cards");
-        
+
         categoryCards.forEach(categoryCard => {
             categoryCard.addEventListener("click", function () {
-                eventCards.style.display = "block"; 
+                categoryCards.forEach(card => {
+                    card.classList.remove("selected");
+                });
+
+                this.classList.add("selected");
+
+                eventCards.style.display = "block";
                 const categoryName = this.classList[1];
                 const categoryEvents = document.querySelectorAll(`.${categoryName}-event`);
-                
+
                 document.querySelectorAll(".event-card").forEach(card => {
                     card.style.display = "none";
                 });
-                
+
                 categoryEvents.forEach(event => {
                     event.style.display = "block";
                 });
             });
         });
-    
+
         const academicsCategoryCard = document.querySelector(".academics");
         academicsCategoryCard.click();
     });
