86fb67bc905ea220620861749355030c16636c07
ffutera <ffutera@gmail.com>
2023-08-19 18:31:59 +0100

    use better style for js

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const categoryCards = document.querySelectorAll(".category-card");
-    const eventCards = document.querySelector(".event-cards");
-    
-    categoryCards.forEach(categoryCard => {
-        categoryCard.addEventListener("click", function () {
-            eventCards.style.display = "block"; 
-            const categoryName = this.classList[1];
-            const categoryEvents = document.querySelectorAll(`.${categoryName}-event`);
-            
-            document.querySelectorAll(".event-card").forEach(card => {
-                card.style.display = "none";
-            });
-            
-            categoryEvents.forEach(event => {
-                event.style.display = "block";
+(function () {
+    document.addEventListener("DOMContentLoaded", function () {
+        const categoryCards = document.querySelectorAll(".category-card");
+        const eventCards = document.querySelector(".event-cards");
+        
+        categoryCards.forEach(categoryCard => {
+            categoryCard.addEventListener("click", function () {
+                eventCards.style.display = "block"; 
+                const categoryName = this.classList[1];
+                const categoryEvents = document.querySelectorAll(`.${categoryName}-event`);
+                
+                document.querySelectorAll(".event-card").forEach(card => {
+                    card.style.display = "none";
+                });
+                
+                categoryEvents.forEach(event => {
+                    event.style.display = "block";
+                });
             });
         });
+    
+        const academicsCategoryCard = document.querySelector(".academics");
+        academicsCategoryCard.click();
     });
-
-    const academicsCategoryCard = document.querySelector(".academics");
-    academicsCategoryCard.click();
-});
+})();
