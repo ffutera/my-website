10661fb3b8f0adce6799e195c85326639490193f
ffutera <ffutera@gmail.com>
2023-08-19 18:29:46 +0100

    Fill in website

--- index.js
+++ index.js
@@ -1,55 +1,23 @@
-(function () {
-  let slideIndex = 0;
+document.addEventListener("DOMContentLoaded", function () {
+    const categoryCards = document.querySelectorAll(".category-card");
+    const eventCards = document.querySelector(".event-cards");
+    
+    categoryCards.forEach(categoryCard => {
+        categoryCard.addEventListener("click", function () {
+            eventCards.style.display = "block"; 
+            const categoryName = this.classList[1];
+            const categoryEvents = document.querySelectorAll(`.${categoryName}-event`);
+            
+            document.querySelectorAll(".event-card").forEach(card => {
+                card.style.display = "none";
+            });
+            
+            categoryEvents.forEach(event => {
+                event.style.display = "block";
+            });
+        });
+    });
 
-  function showSlides() {
-    const slides = document.getElementsByClassName("slide");
-    const sliderControls = document.getElementsByClassName("slider-controls");
-    if (slides.length > 1) {
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) {
-        slideIndex = 1;
-      }
-      slides[slideIndex - 1].style.display = "block";
-    } else {
-      sliderControls[0].style.display = "none";
-    }
-    setTimeout(showSlides, 5000); 
-  }
-
-  function prevSlide() {
-    slideIndex--;
-    const slides = document.getElementsByClassName("slide");
-    if (slideIndex < 1) {
-      slideIndex = slides.length;
-    }
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
-  }
-
-  function nextSlide() {
-    slideIndex++;
-    const slides = document.getElementsByClassName("slide");
-    if (slideIndex > slides.length) {
-      slideIndex = 1;
-    }
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
-  }
-
-  function initSlider() {
-    showSlides();
-    const prevBtn = document.querySelector(".prev");
-    const nextBtn = document.querySelector(".next");
-    prevBtn.addEventListener("click", prevSlide);
-    nextBtn.addEventListener("click", nextSlide);
-  }
-
-  initSlider();
-})();
+    const academicsCategoryCard = document.querySelector(".academics");
+    academicsCategoryCard.click();
+});
